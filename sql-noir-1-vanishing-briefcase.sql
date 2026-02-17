7506eb9b12f35198b23ea5349a9073abe1ebfcd2
meganirenegott <meganirenegott@gmail.com>
2026-02-17 09:50:53 -0800

    clue 2 suspects

--- sql-noir-1-vanishing-briefcase.sql
+++ sql-noir-1-vanishing-briefcase.sql
@@ -3,7 +3,7 @@
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
 
-
+-- query
 SELECT * FROM crime_scene WHERE location = 'Blue Note Lounge';
 
 -- result 
@@ -14,3 +14,33 @@ description = "a briefcase containing sensitive documents vanished. A
 witness reported a man in a trench coat with a scar on his left cheek fleeing
 the scene"
 
+-- query
+SELECT * FROM suspects WHERE id = 76;
+
+-- result 
+id = 76
+name = emma hall
+attire = jacket 
+scar = none
+
+
+-- query
+SELECT * FROM interviews WHERE suspect_id = 76;
+
+-- result 
+supect_id = 76
+transcript = null 
+
+-- query
+SELECT * FROM suspects WHERE attire = 'trench coat' AND scar = 'left cheek';
+
+-- results 
+id = 3
+name = Frankie Lombardi 
+attire = trench coat
+scar = left cheek 
+
+id = 183
+name = Vincent Malone
+attire = trench coat
+scar = left cheek 
