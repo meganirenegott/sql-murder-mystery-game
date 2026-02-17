041a76f5e431b30e22b41cfff69ac5220154cb6d
meganirenegott <meganirenegott@gmail.com>
2026-02-17 10:05:47 -0800

    crime scene report for case 2

--- sql-noir-2-stolen-sound.sql
+++ sql-noir-2-stolen-sound.sql
@@ -2,3 +2,29 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+-- query
+SELECT * FROM crime_scene
+WHERE date = 19830715
+AND location = 'West Hollywood Records';
+
+-- result 
+id = 65
+date = 19830715
+type = theft
+location = West Hollywood Records
+description = a prized vinyl record was stolen from the store during a busy evening 
+
+-- query
+SELECT * FROM witnesses
+WHERE crime_scene_id = 65;
+
+-- result
+
+id = 28
+crime_scene_id = 65 
+clue = I saw a man wearing a red bandana rushing out of the store
+
+id = 75
+crime_scene_id = 65 
+clue = The main thing I remember is that he had a distinctive gold watch on his wrist
